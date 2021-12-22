96b74f733b8a1d4e1ff496095ce9f5e48d7187e3
Aksh Sood <sood_aksh_2001@outlook.com>
2021-12-22 06:46:03 +0530

    promises done

--- 3.js
+++ 3.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 const superagent = require("superagent");
+
 const readFilePro = (file) => {
   return new Promise((resolve, reject) => {
     fs.readFile(file, (err, data) => {
@@ -18,21 +19,67 @@ const writeFilePro = (path, data) => {
   });
 };
 
-readFilePro("resource/complete-node-bootcamp/3-asynchronous-JS/starter/dog.txt")
-  .then((data) => {
-    console.log(`Breed: ${data}`);
-    return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
-  })
-  .then((res) => {
-    console.log(res.body.message);
-    return writeFilePro(
-      "resource/complete-node-bootcamp/3-asynchronous-JS/starter/dog-img.txt",
-      res.body.message
+const getDogPic = async () => {
+  try {
+    const data = await readFilePro(
+      "resource/complete-node-bootcamp/3-asynchronous-JS/starter/dog.txt"
     );
-  })
-  .then((data) => {
-    console.log(data);
-  })
-  .catch((err) => {
-    console.log(err.message);
-  });
+    console.log(`Breed: ${data}`);
+    const res = superagent.get(
+      `https://dog.ceo/api/breed/${data}/images/random`
+    );
+    const res1 = superagent.get(
+      `https://dog.ceo/api/breed/${data}/images/random`
+    );
+    const res2 = superagent.get(
+      `https://dog.ceo/api/breed/${data}/images/random`
+    );
+    const all = await Promise().all([res, res1, res2]);
+    const imgs = all.map((el) => el.body.message);
+    console.log(imgs);
+
+    await writeFilePro(
+      "resource/complete-node-bootcamp/3-asynchronous-JS/starter/dog-img.txt",
+      imgs.join("\n")
+    );
+    console.log("File Saved");
+  } catch (err) {
+    console.log(err);
+    throw err;
+  }
+  return "2:READY";
+};
+
+(async () => {
+  try {
+    console.log("1: Will get dog pics!");
+    const x = await getDogPic();
+    console.log(x);
+    console.log("3: Done getting dog pics!");
+  } catch (err) {
+    console.log("ERROR 💥");
+  }
+})();
+
+// getDogPic().then((x) => {
+//   console.log(x);
+//   console.log("3: Done getting done");
+// });
+// readFilePro("resource/complete-node-bootcamp/3-asynchronous-JS/starter/dog.txt")
+//   .then((data) => {
+//     console.log(`Breed: ${data}`);
+//     return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
+//   })
+//   .then((res) => {
+//     console.log(res.body.message);
+//     return writeFilePro(
+//       "resource/complete-node-bootcamp/3-asynchronous-JS/starter/dog-img.txt",
+//       res.body.message
+//     );
+//   })
+//   .then((data) => {
+//     console.log(data);
+//   })
+//   .catch((err) => {
+//     console.log(err.message);
+//   });
